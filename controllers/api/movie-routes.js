9b2b7a7ac24cf62af53780f73d418bb9ca3b0a4a
Victoria Landron <viclandron@gmail.com>
2020-11-15 17:56:42 -0500

    getting api to display 2

--- controllers/api/movie-routes.js
+++ controllers/api/movie-routes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const axios = require('axios');
 
+
 router.get('/', (req, res) => {
     axios.get("http://www.omdbapi.com/?t=Toy+Story&apikey=d69ff318")
         .then(function (response) {
-            console.log([
+            const movies = [
                 response.data.Title,
                 response.data.Poster
-            ]);
+            ]
         })
         .catch(function (error) {
             // handle error
