ee8e0372571a849a2799c78832e274d9a511712b
Victoria Landron <viclandron@gmail.com>
2020-11-15 18:18:37 -0500

    getting a response error

--- controllers/api/movie-routes.js
+++ controllers/api/movie-routes.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 
 
 router.get('/', (req, res) => {
-    axios.get("http://www.omdbapi.com/?t=Toy+Story&apikey=d69ff318")
-        .then(function (response) {
-            const movies = [
-                response.data.Title,
-                response.data.Poster
-            ]
+    axios.get("http://www.omdbapi.com/?t=Toy+Story&apikey=d69ff318")  
+    .then(function (response) {
+            const movies = {
+                title: response.data.Title,
+                poster: response.data.Poster
+            }
         })
         .catch(function (error) {
             // handle error
--- public/javascript/movies.js
+++ public/javascript/movies.js
@@ -2,8 +2,8 @@ async function getMovies() {
     const response = await fetch('/api/movie', {
         method: 'get',
         body: JSON.stringify({
-            poster: response.data.Poster,
-            title: response.data.Title
+            title,
+            poster
         }),
         headers: { 'Content-Type': 'application/json' }
     })
